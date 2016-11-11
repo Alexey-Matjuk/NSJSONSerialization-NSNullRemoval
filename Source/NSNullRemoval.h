52fb045d15b6d62c257ba0d455d9133a8b064670
Alexey Matjuk <alexey.matjuk@itechart-group.com>
2016-11-11 18:00:58 +0300

    Adds umbrela header http://stackoverflow.com/a/30815500/2083655

---
+++ Source/NSNullRemoval.h
@@ -0,0 +1,14 @@
+//
+//  NSNullRemoval.h
+//  NSNullRemovalTest
+//
+//  Created by Alexey Matjuk on 11/11/16.
+//  Copyright © 2016 Vertical Turtle. All rights reserved.
+//
+
+#ifndef NSNullRemoval_h
+#define NSNullRemoval_h
+
+#import "NSJSONSerialization+RemovingNulls.h"
+
+#endif /* NSNullRemoval_h */
