d8e2170056e1dff2dd0f697c8ba4ed2dfdb4a930
Alexey Matjuk <alexey.matjuk@itechart-group.com>
2016-11-11 19:24:09 +0300

    Updates headers

--- Source/NSNullRemoval.h
+++ Source/NSNullRemoval.h
@@ -6,6 +6,14 @@
 //  Copyright © 2016 Vertical Turtle. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSJSONSerialization+RemovingNulls iOS.
+FOUNDATION_EXPORT double NSNullRemoval_VersionNumber;
+
+//! Project version string for NSJSONSerialization+RemovingNulls iOS.
+FOUNDATION_EXPORT const unsigned char NSNullRemoval_VersionString[];
+
 #ifndef NSNullRemoval_h
 #define NSNullRemoval_h
 
